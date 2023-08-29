560de9f5859cb1fcac142cee8822796fa02e70c0
Arch-Noize <santiagonoize@gmail.com>
2023-08-29 12:06:55 -0400

    Add function

---
+++ src/store.js
@@ -0,0 +1,10 @@
+/* eslint-disable import/prefer-default-export */
+
+import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './features/cart/cartSlice';
+
+export const store = configureStore({
+  reducer: {
+    cart: cartReducer,
+  },
+});
